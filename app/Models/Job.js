e62f32436c89b53e2241cd973dbecb26b46fcd63
Ben W <bwiedeback@gmail.com>
2020-08-26 13:58:07 -0600

    updates to store, models

---
+++ app/Models/Job.js
@@ -0,0 +1,33 @@
+import { generateId } from "../utils.js";
+console.log(0)
+export default class Car {
+  // Object Destructuring
+  constructor({ position, salary, posted, company, img, description }) {
+    console.log(2);
+    this.id = generateId()
+    this.position = position
+    this.salary = salary
+    this.posted = posted
+    this.company = company
+    this.img = img
+    this.description = description || "no description"
+  }
+
+  get Template() {
+    return `            
+    <div class="col-12">
+      <div class="card">
+          <img class="card-img-top" src="${this.img}" alt="">
+          <div class="card-body">
+              <h4 class="card-title">${this.company} - ${this.position} - ${this.salary}</h4>
+              <p class="card-text">${this.description}</p>
+              <div class="d-flex justify-content-between">
+                  <button class="btn btn-outline-danger" onclick="app.carsController.removeCar('${this.id}')">Delete</button>
+                  <button class="btn btn-outline-info" onclick="app.carsController.bid('${this.id}')">+ $100</button>
+                  <p>Posted: ${this.posted}</p>
+              </div>
+          </div>
+      </div>
+    </div>`
+  }
+}
--- app/store.js
+++ app/store.js
@@ -1,4 +1,6 @@
 import Car from "./Models/Car.js";
+import House from "./Models/House.js";
+import Job from "./Models/Job.js";
 console.log(1);
 
 let _state = {
@@ -8,7 +10,16 @@ let _state = {
     new Car({ make: "Mystery", model: "Machine", year: 1987, price: 1000, img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-classic-mystery-machine-replica-van-built-by-jerry-news-photo-1587131341.jpg?crop=1.00xw:0.753xh;0,0.176xh&resize=1200:*", description: "Smells like meddling teenagers and a dog" }),
     new Car({ make: "Jeep", model: "Wrangler", year: 1987, price: 1000, img: "https://vignette.wikia.nocookie.net/jurassicpark/images/e/e7/Jurassic-park-jeep3-1-.jpg/revision/latest/scale-to-width-down/340?cb=20090417212624", description: "Are those teeth marks?" })
   ],
-  houses: []
+  /** @type {House[]} */
+  houses: [
+    new House({ beds: "10", baths: "5", yearbuilt: "1785", price: "10,000,000", img: "https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2018/07/french-villa-sm.jpg", description: "This lovely seaside villa is your large slice of paradise in France." }),
+    new House({ beds: "5", baths: "3", yearbuilt: "1983", price: "100,000", img: "https://bloximages.chicago2.vip.townnews.com/rexburgstandardjournal.com/content/tncms/assets/v3/editorial/3/73/373595c6-2445-11e0-8a06-001cc4c002e0/4d37aba6c3f39.image.jpg", description: "Discounted for quick sale.  May contain snakes." }),
+    new House({ beds: "10", baths: "5", yearbuilt: "1785", price: "10,000,000", img: "https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2018/07/french-villa-sm.jpg", description: "This lovely seaside villa is your large slice of paradise in France." })
+  ],
+  /** @type {Job[]} */
+  jobs: [
+
+  ],
 };
 
 class Store {
